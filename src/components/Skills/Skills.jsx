61ab3b1fc5cd75fed2b262a2d28151d85ed4e269
imabwahab <imabwahab@gmail.com>
2025-06-12 17:41:17 +0500

    Enhances the skills component.

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,42 +1,101 @@
 import styles from './Skills.module.css'
-import React from 'react'
-import UIDesign from '../../assets/ui-design.png'
-import WebDesign from '../../assets/website-design.png'
-import AppDesign from '../../assets/app-design.png'
+import { FaHtml5 } from "react-icons/fa6";
+import { FaCss3Alt } from "react-icons/fa6";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaBootstrap } from "react-icons/fa6";
+import { FaReact } from "react-icons/fa6";
+import { BsFiletypeSql } from "react-icons/bs";
+import { BiLogoMongodb } from "react-icons/bi";
 
 function Skills() {
   return (
     <section id={styles.skills}>
-      <span className={styles.skillTitle}>What I do</span>
-      <span className={styles.skillDesc}>I'm a skilled front-end web developer with experience in building visually appealing,<br /> user-friendly websites. I specialize in designing and developing responsive web solutions <br />using HTML, CSS, JavaScript, React, and Bootstrap, ensuring seamless user experiences.
+      <span className={styles.skillTitle}>Skills</span>
+      <span className={styles.skillDesc}>
+        My expertise spans HTML, CSS, JavaScript, React, Bootstrap, and databases like MySQL and MongoDB. Below, you'll find a visual breakdown of my proficiency in each technology.
       </span>
 
+
       <div className={styles.skillsBar}>
-        <div className={styles.skillBar}>
-          <img src={UIDesign} alt="UIDesign" className={styles.skillBarImg} />
+
+        <div className={styles.skillBox}>
+          <FaHtml5 className={styles.icon} />
           <div className={styles.skillBarText}>
-            <h2>UI/UX design</h2>
-            <p>This is a demo text, you can write your own content here</p>
+            <h2>HTML</h2>
+            <div className={styles.progressBar}>
+              <div className={styles.progressFill} style={{ width: '70%' }}></div>
+            </div>
+            <span className={styles.percentage}>70%</span>
           </div>
         </div>
 
-        <div className={styles.skillBar}>
-          <img src={WebDesign} alt="WebDesign" className={styles.skillBarImg} />
+        <div className={styles.skillBox}>
+          <FaCss3Alt className={styles.icon} />
           <div className={styles.skillBarText}>
-            <h2>Website Design</h2>
-            <p>This is a demo text, you can write your own content here</p>
+            <h2>CSS</h2>
+            <div className={styles.progressBar}>
+              <div className={styles.progressFill} style={{ width: '70%' }}></div>
+            </div>
+            <span className={styles.percentage}>70%</span>
           </div>
         </div>
 
-        <div className={styles.skillBar}>
-          <img src={AppDesign} alt="AppDesign" className={styles.skillBarImg} />
+        <div className={styles.skillBox}>
+          <IoLogoJavascript className={styles.icon} />
           <div className={styles.skillBarText}>
-            <h2>App Design</h2>
-            <p>This is a demo text, you can write your own content here</p>
+            <h2>JavaScript</h2>
+            <div className={styles.progressBar}>
+              <div className={styles.progressFill} style={{ width: '70%' }}></div>
+            </div>
+            <span className={styles.percentage}>70%</span>
+          </div>
+        </div>
+
+
+        <div className={styles.skillBox}>
+          <FaBootstrap className={styles.icon} />
+          <div className={styles.skillBarText}>
+            <h2>Bootstrap</h2>
+            <div className={styles.progressBar}>
+              <div className={styles.progressFill} style={{ width: '70%' }}></div>
+            </div>
+            <span className={styles.percentage}>70%</span>
+          </div>
+        </div>
+
+        <div className={styles.skillBox}>
+          <FaReact className={styles.icon} />
+          <div className={styles.skillBarText}>
+            <h2>REACT</h2>
+            <div className={styles.progressBar}>
+              <div className={styles.progressFill} style={{ width: '70%' }}></div>
+            </div>
+            <span className={styles.percentage}>70%</span>
+          </div>
+        </div>
+
+        <div className={styles.skillBox}>
+          <BsFiletypeSql className={styles.icon} />
+          <div className={styles.skillBarText}>
+            <h2>MySQL</h2>
+            <div className={styles.progressBar}>
+              <div className={styles.progressFill} style={{ width: '70%' }}></div>
+            </div>
+            <span className={styles.percentage}>70%</span>
+          </div>
+        </div>
+
+        <div className={styles.skillBox}>
+          <BiLogoMongodb className={styles.icon} />
+          <div className={styles.skillBarText}>
+            <h2>MongoDB</h2>
+            <div className={styles.progressBar}>
+              <div className={styles.progressFill} style={{ width: '70%' }}></div>
+            </div>
+            <span className={styles.percentage}>70%</span>
           </div>
         </div>
       </div>
-
     </section>
   )
 }
