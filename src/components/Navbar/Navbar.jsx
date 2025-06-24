d1594d243d5ba030f8df79b34bc926febc63097c
imabwahab <imabwahab@gmail.com>
2025-06-25 01:37:25 +0500

    .

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 
 import W from '../../assets/W.png'
-import contactImg from '../../assets/contact.png'
 
+import { HiOutlineDocumentText } from "react-icons/hi";
 import '../Navbar/Navbar.css'
 import { Link } from 'react-scroll'
 import menu from '../../assets/menu.png'
@@ -18,22 +18,24 @@ function Navbar() {
         <Link className="desktopMenuItem" activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500}>Home</Link>
         <Link className="desktopMenuItem" activeClass='active' to='skills' spy={true} smooth={true} offset={-40} duration={500}>Skills</Link>
         <Link className="desktopMenuItem" activeClass='active' to='works' spy={true} smooth={true} offset={-80} duration={500} >Projects</Link>
-       
+        <Link className="desktopMenuItem" activeClass='active' to='contactPage' spy={true} smooth={true} offset={-80} duration={500} >Contact</Link>
+
       </div>
 
-      <button className="desktopMenuBtn" onClick={() => {
-        document.getElementById('contactPage').scrollIntoView({ behavior: 'smooth' });
-      }}>
-        <img src={contactImg} alt="" className='desktopMenuImg' />
-        Contact Me
-      </button>
+      <a className='download-link ' href="/portfolio/cv.pdf" download="cv.pdf">
+        <button className="btn"><HiOutlineDocumentText /> CV</button>
+      </a>
 
 
       <img src={coloredmenu} className='mobMenu' onClick={() => setShowMenu(!showMenu)} />
       <div className="navMenu" style={{ display: showMenu ? 'flex' : 'none' }}>
+        
         <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={() => setShowMenu(false)}>Home</Link>
+
         <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-50} duration={500} className="listItem" onClick={() => setShowMenu(false)}>Skills</Link>
+
         <Link activeClass='active' to='works' spy={true} smooth={true} offset={-50} duration={500} className="listItem" onClick={() => setShowMenu(false)}>Projects</Link>
+
         <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-50} duration={500} className="listItem" onClick={() => setShowMenu(false)}>Contact</Link>
       </div>
     </nav>
