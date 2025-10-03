97c92a9c3aaf1cfe4c6ca69d19bab95536bc1681
imabwahab <imabwahab@gmail.com>
2025-10-03 22:47:58 +0500

    styled the component.

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   const [darkMode, setDarkMode] = useState(false);
   return (
-    <div className={`${darkMode ? 'dark' : ''} `}>
+    <div className={`${!darkMode ? 'dark' : ''} `}>
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
       <div className=' pt-16 lg:px-10 xl:px-15 bg-gray-100 dark:bg-dark-midnight transition-all duration-300'>
         <HeroSection />
